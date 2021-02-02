a126bcccd729b59f227040158a063b809888ea16
Rodrigo Castillo <53545808+r0g0o@users.noreply.github.com>
2021-02-01 22:34:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,8 +76,6 @@ def main():
                           url_path=TOKEN)
     updater.bot.setWebhook('https://yinidijiisibot.herokuapp.com/' + TOKEN)
 
-    print('Ejecutandose')
-
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
